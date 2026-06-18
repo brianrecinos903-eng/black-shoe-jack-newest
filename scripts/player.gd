1ef1c7a4dca003c427256a3636b7dbb9a51158bb
brianrecinos903-eng <brianrecinos903@gmail.com>
2026-06-17 23:12:20 -0500

    jack

--- scripts/player.gd
+++ scripts/player.gd
@@ -71,6 +71,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 		JUMP = true
+		
 	
 	#Move
 	var direction := Input.get_axis("left", "right") 
