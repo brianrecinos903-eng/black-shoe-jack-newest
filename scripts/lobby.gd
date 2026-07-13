b90d4da89120b57406cd5ba08d896d73f0cbdefc
brianrecinos903-eng <brianrecinos903@gmail.com>
2026-07-12 21:35:32 -0500

    bongo song

--- scripts/lobby.gd
+++ scripts/lobby.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+@onready var bar_music_: AudioStreamPlayer = $"Bar music!"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	
+	
+	bar_music_.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
