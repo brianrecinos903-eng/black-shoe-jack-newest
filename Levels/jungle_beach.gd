05fd844866631d0a1ddab397c2fc0a7a1cefd0e6
brianrecinos903-eng <brianrecinos903@gmail.com>
2026-07-15 12:40:50 -0500

    seamine addition and music

--- Levels/jungle_beach.gd
+++ Levels/jungle_beach.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
+@onready var beach_music: AudioStreamPlayer = $Beach_music
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	beach_music.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
