2225b81b28f3f6826d5a777a191f91d09d5909ac
brianrecinos903-eng <brianrecinos903@gmail.com>
2026-07-15 20:00:19 -0500

    seamine fixed size

--- traps/sea_mine.gd
+++ traps/sea_mine.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	anim.play("idle")
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		body.take_dmg(1, Helpers.DamageType.TRAP)
