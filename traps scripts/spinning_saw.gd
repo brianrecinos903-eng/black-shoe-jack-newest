3aa24294ec2c04960581c7110595d49d1b2454f5
Plumtester-dev <jack.littlefield14@gmail.com>
2026-07-28 15:16:06 -0600

    Added Spinning Saw Obstacle

---
+++ traps scripts/spinning_saw.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+var can_reverse = true
+
+var speed = 500
+var direction = 1
+var spin_speed = 5
+
+func _physics_process(delta: float) -> void:
+	velocity.x = speed * direction
+	rotation += spin_speed * direction * delta
+	
+	move_and_slide()
+	
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		body.take_dmg(1)
+
+func _on_reverse_direction_detect_area_entered(area: Area2D) -> void:
+	if area.is_in_group("reverse_direction") and can_reverse:
+		can_reverse = false
+		direction *= -1
+		print(direction)
+		await get_tree().create_timer(0.2).timeout
+		can_reverse = true
