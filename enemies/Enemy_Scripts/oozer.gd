71ecc689a213ea49e83beaafa1afea745ae427d7
CE-0 <chibs.ego@gmail.com>
2026-07-29 23:44:17 +0100

    Initialized Oozer state setup

--- enemies/Enemy_Scripts/oozer.gd
+++ enemies/Enemy_Scripts/oozer.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
+enum states {
+	ACTIVE, STUNNED
+} 
+
+signal on_player_bounce
 
 func _ready() -> void:
 	anim.play("idle")
