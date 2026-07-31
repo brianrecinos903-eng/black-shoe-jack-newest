d4e2262437fa0f3749163c0f3254da8425605a5a
CE-0 <chibs.ego@gmail.com>
2026-07-31 03:05:27 +0100

    Created Enemy class

--- enemies/Enemy_Scripts/oozer.gd
+++ enemies/Enemy_Scripts/oozer.gd
@@ -1,12 +1,20 @@
-extends CharacterBody2D
+extends Enemy
 
-@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+var direction = 1 
+var spd = 10.0
 
 enum states {
 	ACTIVE, STUNNED
 } 
 
-signal on_player_bounce
+signal on_player_bounce 
+
+func turn(): 
+	direction *= -1
+	pass 
 
 func _ready() -> void:
 	anim.play("idle")
+
+func _process(delta: float) -> void:
+	position.x += spd * direction * delta
